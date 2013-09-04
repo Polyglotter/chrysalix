97ca683372a3444b5078aeadf3130d222c43ffe6
Daniel Florian <dflorian@redhat.com>
2013-09-04 17:25:03 -0400

    Got a simple XFormEngine test working that loads/starts a repository from a configuration file.

---
+++ xform-engine/src/test/java/org/jboss/xform/XFormEngineTest.java
@@ -0,0 +1,39 @@
+/*
+ * See the COPYRIGHT.txt file distributed with this work for information
+ * regarding copyright ownership.
+ *
+ * This software is made available by Red Hat, Inc. under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution and is
+ * available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * See the AUTHORS.txt file in the distribution for a full listing of
+ * individual contributors.
+ */
+package org.jboss.xform;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.junit.Assert.assertThat;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Tests for the {@link XFormEngine}.
+ */
+public final class XFormEngineTest {
+
+    private static final String REPO_CONFIG_FILE = "src/test/resources/config/xformTestRepository.json";
+
+    private XFormEngine engine = null;
+
+    @Before
+    public void constructEngine() {
+        this.engine = new XFormEngine(REPO_CONFIG_FILE);
+    }
+
+    @Test
+    public void shouldObtainSession() throws Exception {
+        assertThat(this.engine.session(), is(notNullValue()));
+    }
+
+}
