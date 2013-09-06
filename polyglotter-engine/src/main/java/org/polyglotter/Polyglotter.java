28a3458468fe98c580c9a42e8fdf0d66ae690bbe
John Verhaeg <john.verhaeg@gmail.com>
2013-09-05 23:10:23 -0500

    Changed how ModeShape is managed since we want to control the repository's lifecycle, and updated Polyglotter test to use custom logger to catch any errors with the metamodel.cnd.

--- polyglotter-engine/src/main/java/org/polyglotter/Polyglotter.java
+++ polyglotter-engine/src/main/java/org/polyglotter/Polyglotter.java
@@ -11,100 +11,82 @@
  */
 package org.polyglotter;
 
-import static org.modeshape.jcr.api.RepositoryFactory.URL;
-
-import java.util.Collections;
-import java.util.Map;
-import java.util.ServiceLoader;
-
 import javax.jcr.Repository;
-import javax.jcr.RepositoryFactory;
 import javax.jcr.Session;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.modeshape.common.collection.Problem;
+import org.modeshape.common.collection.Problems;
+import org.modeshape.common.logging.Logger;
+import org.modeshape.jcr.ModeShapeEngine;
+import org.modeshape.jcr.NoSuchRepositoryException;
+import org.modeshape.jcr.RepositoryConfiguration;
 
 /**
  * 
  */
 public final class Polyglotter {
     
-    private static final Logger LOGGER = LoggerFactory.getLogger( Polyglotter.class );
+    /**
+     * 
+     */
+    public static final String DEFAULT_MODESHAPE_CONFIGURATION_PATH = "jcr/modeShapeConfig.json";
     
-    private final String configurationFilePath;
-    
-    private Repository repository;
-    
+    private String modeShapeConfigurationPath = DEFAULT_MODESHAPE_CONFIGURATION_PATH;
+    private ModeShapeEngine modeShape;
     private Session session;
     
     /**
-     * @param configurationFilePath
+     * @return the path to the ModeShape configuration file. Default is {@value #DEFAULT_MODESHAPE_CONFIGURATION_PATH}.
      */
-    public Polyglotter( final String configurationFilePath ) {
-        this.configurationFilePath = configurationFilePath;
+    public String modeShapeConfigurationPath() {
+        return modeShapeConfigurationPath;
     }
     
     /**
-     * Starts the engine if not already started.
-     * 
      * @return the Polyglotter JCR repository session (never <code>null</code>)
-     * @throws Exception
-     *             if there is a problem obtaining the session
+     * @throws Throwable
      */
-    public Session session() throws Exception {
-        start();
-        
+    public Session session() throws Throwable {
         if ( this.session == null ) {
-            this.session = this.repository.login( "default" );
-            LOGGER.debug( "Polyglotter session started" );
+            modeShape = new ModeShapeEngine();
+            modeShape.start();
+            final RepositoryConfiguration config = RepositoryConfiguration.read( modeShapeConfigurationPath );
+            final Problems problems = config.validate();
+            if ( problems.hasProblems() ) {
+                for ( final Problem problem : problems )
+                    Logger.getLogger( getClass() ).error( problem.getMessage(), problem.getThrowable() );
+                throw problems.iterator().next().getThrowable();
+            }
+            Repository repository;
+            try {
+                repository = modeShape.getRepository( config.getName() );
+            } catch ( final NoSuchRepositoryException err ) {
+                repository = modeShape.deploy( config );
+            }
+            session = repository.login( "default" );
+            Logger.getLogger( getClass() ).info( PolyglotterI18n.polyglotterStarted );
         }
-        
-        return this.session;
+        return session;
     }
     
     /**
-     * Obtains the Polyglotter repository from ModeShape.
-     * 
-     * @throws Exception
-     *             if there is a problem starting Polyglotter
+     * @param modeShapeConfigurationPath
      */
-    public void start() throws Exception {
-        if ( this.repository == null ) {
-            final String configUrl = ( "file:" + this.configurationFilePath );
-            final Map< String, String > params = Collections.singletonMap( URL, configUrl );
-            
-            for ( final RepositoryFactory factory : ServiceLoader.load( RepositoryFactory.class ) ) {
-                this.repository = factory.getRepository( params );
-                
-                if ( this.repository != null ) {
-                    LOGGER.info( "Polyglotter started using configuration file '" + this.configurationFilePath + '\'' );
-                    break;
-                }
-            }
-            
-            if ( this.repository == null ) {
-                throw new Exception( "Polyglotter cannot be started using configuration file '" + this.configurationFilePath + '\'' ); // TODO
-                                                                                                                                       // i18n
-                                                                                                                                       // this
-            }
-        } else {
-            LOGGER.debug( "Attempt to start Polyglotter when it is already started." ); // TODO i18n this
-        }
+    public void setModeShapeConfigurationPath( final String modeShapeConfigurationPath ) {
+        this.modeShapeConfigurationPath =
+            modeShapeConfigurationPath == null ? DEFAULT_MODESHAPE_CONFIGURATION_PATH : modeShapeConfigurationPath;
     }
     
     /**
      * 
      */
     public void stop() {
-        if ( this.repository == null ) LOGGER.debug( "Attempt to stop Polyglotter when it is already stopped" );
+        if ( this.session == null ) Logger.getLogger( getClass() ).debug( "Attempt to stop Polyglotter when it is already stopped" );
         else {
-            if ( this.session != null ) {
-                this.session.logout();
-                this.session = null;
-            }
-            
-            this.repository = null;
-            LOGGER.debug( "Polyglotter stopped" );
+            this.session.logout();
+            this.session = null;
+            this.modeShape = null;
+            Logger.getLogger( getClass() ).info( PolyglotterI18n.polyglotterStopped );
         }
     }
     
